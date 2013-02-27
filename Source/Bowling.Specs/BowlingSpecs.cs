42bcaf1cb0b405d30ae9aa5d5e96109027a3cff6
Jim Ownby <jownby@ownby.net>
2013-02-26 23:55:24 -0500

    Additional Spec coverage
    -2 frames spared
    -10 frames bowled
    -First frame strike

--- Source/Bowling.Specs/BowlingSpecs.cs
+++ Source/Bowling.Specs/BowlingSpecs.cs
@@ -56,9 +56,116 @@ namespace specs_for_bowling
         }
 
         [Specification]
-        public void the_score_is_zero()
+        public void the_score_is_48()
         {
             _score.should_equal(48);
+        }
+
+    }
+    public class when_rolling_2_spare_frames : concerns
+    {
+        private int _score;
+
+        protected override void context()
+        {
+            var game = new Game();
+
+            game.Roll(2);
+            game.Roll(8);
+            game.Roll(2);
+            game.Roll(8);
+
+            16.times(() => game.Roll(2));
+            _score = game.Score();
+        }
+
+        [Specification]
+        public void the_score_is_56()
+        {
+            _score.should_equal(56);
+        }
+
+    }
+
+    public class when_rolling_11th_frame : concerns
+    {
+        private bool _canBowl;
+
+        protected override void context()
+        {
+            var game = new Game();
+            20.times(() => game.Roll(2));
+            _canBowl = game.CanBowl();
+            
+        }
+
+        [Specification]
+        public void cannot_bowl_more()
+        {
+            _canBowl.should_be_false();
+        }
+    }
+
+    public class when_rolling_strike_frame : concerns
+    {
+        private int _score;
+
+        protected override void context()
+        {
+            var game = new Game();
+
+            game.Roll(10);
+            18.times(() => game.Roll(2));
+            _score = game.Score();
+        }
+
+        [Specification]
+        public void the_score_is_50()
+        {
+            _score.should_equal(50);
+        }
+
+    }
+
+    public class when_rolling_2_strike_frames : concerns
+    {
+        private int _score;
+
+        protected override void context()
+        {
+            var game = new Game();
+
+            game.Roll(10);
+            game.Roll(10);
+            16.times(() => game.Roll(2));
+            _score = game.Score();
+        }
+
+        [Specification]
+        public void the_score_is_68()
+        {
+            _score.should_equal(68);
+        }
+
+    }
+
+    public class when_rolling_perfect_game : concerns
+    {
+        private int _score;
+
+        protected override void context()
+        {
+            var game = new Game();
+
+            
+            12.times(() => game.Roll(10));
+            _score = game.Score();
+        }
+
+        [Specification]
+        public void the_score_is_300()
+        {
+            _score.should_equal(300);
         }
 
     }
--- Source/Bowling/Game.cs
+++ Source/Bowling/Game.cs
@@ -10,6 +10,7 @@ namespace Bowling
         private int _score;
         private int _frameCount;
         private bool _isFirstRoll;
+        private int _bonusRoll;
 
         private int[,] frames = new int[10,2];
 
@@ -20,33 +21,84 @@ namespace Bowling
             _isFirstRoll = true;
         }
 
+        private int previousFrame
+        {
+            get { return _frameCount - 1; }
+        }
+
+        private bool lastFrame()
+        {
+            return _frameCount == 9;
+        }
+
+        public bool CanBowl()
+        {
+            return _frameCount < 10 ? true : false;
+        }
+
         public void Roll(int _pins)
         {
-            int _rollScore = _pins;
-
-            if (_isFirstRoll && _frameCount != 0)
+            if (CanBowl())
             {
-                if (IsSpare(frames[(_frameCount - 1), 0], frames[(_frameCount - 1), 1]))
+                int _rollScore = _pins;
+
+                if (_isFirstRoll )
                 {
-                    frames[(_frameCount - 1), 1] += _rollScore;
+                    
+                    if (_frameCount != 0)
+                    {
+                        if (IsStrike(frames[(previousFrame), 0], _isFirstRoll)) //_isFirstRoll slightly redundant here
+                        {
+                            frames[(previousFrame), 1] += (_rollScore);
+                        }
+                        else if (IsSpare(frames[(previousFrame), 0], frames[(previousFrame), 1]))
+                        {
+                            frames[(previousFrame), 1] += _rollScore;
+                        }
+                    }
+                    if (_frameCount > 1 && IsStrike(frames[(previousFrame), 0], _isFirstRoll) && IsStrike(frames[(previousFrame - 1), 0], _isFirstRoll)) 
+                        frames[(previousFrame - 1), 1] += (_rollScore); //2nd strike bonus
+
+                    if (IsStrike(_rollScore, _isFirstRoll)) if (!lastFrame()) _frameCount++;
+                    
                 }
+                else if (!lastFrame())
+                {
+                    if (_frameCount != 0 && IsStrike(frames[(previousFrame), 0], true)) frames[(previousFrame), 1] += (_rollScore); //2nd strike bonus
+
+                    _frameCount++;
+                }
+                else if (lastFrameContinue(frames[(_frameCount), 0],_rollScore))  //10th frame is special
+                {
+
+                }
+
+                int currentFrame = (IsStrike(_rollScore, _isFirstRoll) || !_isFirstRoll) && !lastFrame() ? previousFrame : _frameCount; // because of OoO, need to shift the count if strike or 2nd roll 
+
+                frames[currentFrame, Convert.ToInt32(!_isFirstRoll)] = _rollScore; //flip the bit
+
+                _isFirstRoll = IsStrike(_rollScore, _isFirstRoll) ? true && !lastFrame() : !_isFirstRoll;
             }
-
-            frames[_frameCount, Convert.ToInt32(_isFirstRoll)] = _rollScore;
-
-            
-            if (!_isFirstRoll)
-            {
-                _frameCount++;
-
-            }
-
-            _isFirstRoll = !_isFirstRoll;
         }
 
         private bool IsSpare(int firstRoll, int secondRoll)
         {
             return firstRoll + secondRoll == 10;
+        }
+
+        private bool IsStrike(int rollScore, bool isFirst) //need both values for reuse
+        {
+            return (rollScore == 10 && isFirst);
+            
+        }
+
+        private bool lastFrameContinue(int firstRoll, int secondRoll)
+        {
+            var _continue = lastFrame();
+            _continue = firstRoll == 10;
+            if(!_continue) _continue = secondRoll != 10 ? IsSpare(firstRoll, secondRoll) : true;
+
+            return _continue;
         }
 
         public int Score()
